4addb7b8838ced0e8cc4218e08c23a26c0376633
Henry <henrygraves95@gmail.com>
2021-03-11 16:31:19 -0800

    renamed includes/ to source/

--- includes/random.js
+++
@@ -1,8 +0,0 @@
-/**
- * @name random.js
- * @brief A function for generating pseudo random integers
- */
-
-module.exports = getRandomInt = (max) => {
-    return Math.floor(Math.random() * max)
-}
--- includes/speak.js
+++
@@ -1,6 +0,0 @@
-/**
- * @name speak.js
- * @date 2.6.21
- * @description function for speaking in tts using say package
- */
-
