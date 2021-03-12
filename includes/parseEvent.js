2892d94256dc5bf4a31541e0c4cc7578d5aba00c
Henry <henrygraves95@gmail.com>
2021-03-11 16:23:21 -0800

    add voice command logic to parse event

--- includes/parseEvent.js
+++ includes/parseEvent.js
@@ -7,6 +7,9 @@
  */
 const scrape = require('./scrape.js')
 const play = require('./play.js')
+const fs = require('fs')
+let dir = 'C:/Users/Vex/Google Drive/KawaiiBotSounds'
+const discord = require('discord.js')
 /**
  * processEvent function, determines the nature of the
  * event and processes accordingly.
@@ -21,12 +24,48 @@ module.exports = parseEvent = async (event) => {
         // declare / set vars
         let tag = ''
         let author = '<@'+event.author.id+'>'
-
+        let files = fs.readdirSync(dir)
+        let help = ""
         // confirmed command attempt
-        // populate files from shared dir
-        
+        // populate files from shared dir for voice commands
+        for (file in files) {
+            if (event.content == './help') {
+                help += './'+files[file].split('.')[0]+'\n'
+                sendHelp = true
+                
+            } else if (files[file].split('.')[0] == event.content.split(' ')[0].split('/')[1]) {
+                console.log(files[file])
+                voiceCommand = true
+                if (event.member.voice.channel) {
+                    play(
+                        `C:/Users/Vex/Google Drive/KawaiiBotSounds/${files[file]}`, 
+                        event.member.voice.channel
+                        ).then((result) => {
+                            result[0].play(result[1])
+                    })
+                    event.delete().catch((error) => {
+                        console.error(error)
+                    })
+                    return
+                } else {
+                    event.channel.send('Why are you so null dude? :\\')
+                    break
+                }
+            }
+        }
+        if (sendHelp) {
+            let msg = new discord.MessageEmbed()
+                .setTitle("Voice Channel Commands:")
+                .setDescription(help)
+                .setThumbnail('http://cdn.discordapp.com/avatars/740565901522239510/029c236d99de4c8cec13e982471f6e60.png')
+            event.author.send(msg)
+            event.delete().catch((error) => {
+                console.error(error)
+            })
+            return
+        }
 
-        // if mentioned, grab the @
+        // if mentioned, grab the @ for text commands
         for (arg in args) {
             if (args[arg].includes('<@!'))
                 tag = args[arg]
