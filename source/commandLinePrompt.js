3f2eae2f3fd602c5bdc80602f365d88aeecb9954
Henry Graves <henrygraves95@gmail.com>
2021-07-24 01:51:33 -0700

    using readline instead of prompt

--- source/commandLinePrompt.js
+++ source/commandLinePrompt.js
@@ -1,11 +1,20 @@
-const prompt            = require('prompt')
+const readline = require('readline')
 
 /**
  * Only purpose is to provide a prompt in command line
  * while the bot is running. May add commands in the
  * future.
  */
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 while(true) {
     console.log(">>")
-    prompt('')
+    rl.question('', () => {
+        rl.close()
+    })
+    rl.on('close', () => {
+        process.exit(0)
+    })
 }
