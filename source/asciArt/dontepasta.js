9ebf9b255e655749084e3b35de63d608edf4cc6e
Henry Graves <henrygraves95@gmail.com>
2021-09-03 20:40:17 -0700

    add donte pasta file

---
+++ source/asciArt/dontepasta.js
@@ -0,0 +1,25 @@
+module.exports = dontepasta = () => {
+    const pasta = 
+`
+But currently I'm not interested in others. <name>, you agree with
+the statistics which tells that men once in a while jerk? Your
+penchant is similar to drug addiction. It might even be said that
+u're sick. We're gonna speak specifically about u, since the
+blackmail material with this habit is only regarding you. You may
+try to implore me, but on emailing this notice I'll delete this
+address. Perhaps I'll provide you with forty-five hours to satisfy
+my requirements. My requirements are extremely simple, taking
+into consideration the current state of affairs 1249 $, LiteCoin
+LZ2TGQVxJFMCnaNaDyL5tiAxapEti2D3TW. So that you 
+understand the scope of this problem, I saved the contacts from
+your mail and I am going to share your video clip with your loved
+ones. I do not know yet the additional way I'm gonna share your 
+clip. If you flout me, in 90 hours your prestige is going to be
+annihilated in the sight of humans. I would like to give you a
+piece of advice to stick up the web camera of your device when
+u do this sort of stuff. Nobody can guarantee that ur device is
+protected against outside interference. Fidelity to your lover is
+the best protection against the situations like that.
+`
+    return pasta
+}
