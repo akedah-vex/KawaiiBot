b1fdba5674925786b64780b25aba2a5311c04958
Henry Graves <henrygraves95@gmail.com>
2021-09-27 20:32:44 -0700

    add error handling for dming users

--- source/kawaiibot.js
+++ source/kawaiibot.js
@@ -60,23 +60,28 @@ module.exports = Kawaiibot = class {
                 }).catch(error => { console.error("ERROR: ", error)} )
                 deleteEvent(event) // delete the command from the chat
             } else if (event.channel.type == "dm") {
-                if (event.content.includes('new') && 
-                    event.content.includes('world') && 
-                    event.content.includes('role') && event.author.id != '740565901522239510') {
-                    let guild = this.client.guilds.cache.find(({name}) => name === "Deimos Esports Community")
-                    let role = guild.roles.cache.find(({name}) => name === '👻 New World Members')
-                    let member = guild.members.cache.find(
-                        ({user: {username, discriminator}}) =>
-                            `${username}#${discriminator}` === event.author.username + "#" + event.author.discriminator,
-                    )
-                    member.roles.add(role);
-                    event.author.send('New world role added! Have fun!')
-                    console.log("Added new world role to: ", event.author.username + "#" + event.author.discriminator)
-                } else if (event.content.includes('new') && event.content.includes('world')) {
-                    event.author.send("If you want the new world members role, make sure you say new world role in your message!")
-                } else {
-                    clever(event, event.channel.type)
+                try {
+                    if (event.content.includes('new') && 
+                        event.content.includes('world') && 
+                        event.content.includes('role') && event.author.id != '740565901522239510') {
+                        let guild = this.client.guilds.cache.find(({name}) => name === "Deimos Esports Community")
+                        let role = guild.roles.cache.find(({name}) => name === '👻 New World Members')
+                        let member = guild.members.cache.find(
+                            ({user: {username, discriminator}}) =>
+                                `${username}#${discriminator}` === event.author.username + "#" + event.author.discriminator,
+                        )
+                        member.roles.add(role);
+                        event.author.send('New world role added! Have fun!')
+                        console.log("Added new world role to: ", event.author.username + "#" + event.author.discriminator)
+                    } else if (event.content.includes('new') && event.content.includes('world')) {
+                        event.author.send("If you want the new world members role, make sure you say new world role in your message!")
+                    } else {
+                        clever(event, event.channel.type)
+                    }
+                } catch (exception) {
+                    console.log(exception)
                 }
+                
             } else if (mentioned(event) == this.id) {
                 clever(event, event.channel.type)
             }
