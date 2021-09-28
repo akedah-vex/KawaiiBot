a74a8029badc03a2c26d6617e3fd89f1bc9e6a68
Henry Graves <henrygraves95@gmail.com>
2021-09-27 18:54:59 -0700

    add member searching for new world role add

--- source/kawaiibot.js
+++ source/kawaiibot.js
@@ -63,8 +63,11 @@ module.exports = Kawaiibot = class {
                 if (event.content.includes('new') && event.content.includes('world') && event.content.includes('role')) {
                     let guild = this.client.guilds.cache.find(({name}) => name === "Deimos Esports Community")
                     let role = guild.roles.cache.find(({name}) => name === 'New World Members')
-                    
-                    event.author.roles.add(role);
+                    let member = guild.members.cache.find(
+                        ({user: {username, discriminator}}) =>
+                            `${username}#${discriminator}` === event.author.username + "#" + event.author.discriminator,
+                    )
+                    member.roles.add(role);
                     event.author.send('New world role added! Have fun!')
                 } else if (event.content.includes('new') && event.content.includes('world')) {
                     event.author.send("If you want the new world members role, make sure you say new world role in your message!")
